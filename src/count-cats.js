737abc52c20d36a116088111de78e3282cc23158
Zaikou Eugene <evgeniikas9@gmail.com>
2021-09-14 23:06:49 +0300

    feat: did carbon-dating and count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats( matrix ) {
+  let count = 0;
+  for(let m of matrix) {
+    for(let l of m) {
+      if(l === '^^') {
+
+        count++;
+      }
+    }
+  }
+  return count;
 }
--- src/dream-team.js
+++ src/dream-team.js
@@ -16,4 +16,5 @@ import { NotImplementedError } from '../extensions/index.js';
 export default function createDreamTeam(/* members */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  
 }
