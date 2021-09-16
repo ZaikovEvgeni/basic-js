881b49f24efea1e246fa69ac8a80ea8c98f8015f
Zaikou Eugene <evgeniikas9@gmail.com>
2021-09-16 22:53:19 +0300

    feat: did hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi( disksNumber, turnsSpeed ) {
+  let oneTurnDisk = turnsSpeed / 3600;    
+  console.log(oneTurnDisk);
+  let i = 1;
+  let k = 2;
+
+  let count = 0;
+hanoi(disksNumber, i, k);
+
+  function hanoi(disksNumber, i, k) {
+      count++;
+      if (disksNumber === 1) {
+          return 1;
+      } else {
+         
+          let tmp = 6 - i - k;
+          hanoi(disksNumber - 1, i, tmp);
+          hanoi(disksNumber - 1, tmp, k)
+
+      }
+     
+  }
+  
+  return {
+      turns: count,
+      seconds: Math.floor(count / turnsSpeed * 3600),
+  }
 }
