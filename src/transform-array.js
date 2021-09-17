b1e1c00f86893c85db3ea5583fef4c471ec37754
Zaikov Evgeni <evgeniikas9@gmail.com>
2021-09-17 15:38:11 +0300

    feat: did change array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,74 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let newArr = [];
+  if (!(Array.isArray(arr))) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  else if (arr.length === 0) {
+    return [];
+  }
+  else {
+    
+    newArr = arr.slice();
+    for (let index = 0; index < newArr.length; index++) {
+      if (newArr[index] === '--discard-next' || newArr[index] === '--discard-prev'
+        || newArr[index] === '--double-next' || newArr[index] === '--double-prev') {
+        switch (newArr[index]) {
+          case '--discard-next':
+            if (index <= newArr.length) {
+              if (index < newArr.length - 1) {
+                newArr.splice(index, 2);
+                if (newArr[index] == '--double-prev' || newArr[index] == '--discard-prev') {
+                  newArr.splice(index, 1);
+                }
+
+              } else {
+                newArr.splice(index, 1);
+
+              }
+            }
+            index = 0;
+            break;
+          case '--discard-prev':
+            if (index >= 0) {
+              if (index === 0) {
+                newArr.splice(index, 1);
+              } else {
+
+                newArr.splice(index - 1, 2);
+              }
+            }
+            index = 0;
+            break;
+          case '--double-next':
+            if (index <= newArr.length - 1) {
+              if (index === newArr.length - 1) {
+                newArr.splice(index, 1);
+              } else {
+                newArr.splice(index, 1);
+                newArr.splice(index, 0, newArr[index])
+              }
+            }
+            index = 0;
+            break;
+          case '--double-prev':
+            if (index >= 0) {
+              if (index === 0) {
+                newArr.splice(index, 1);
+              } else {
+                newArr.splice(index, 1);
+                newArr.splice(index, 0, newArr[index - 1])
+
+              }
+              index = 0;
+              break;
+            }
+        }
+      }
+    }
+   
+    return newArr;
+  }
 }
