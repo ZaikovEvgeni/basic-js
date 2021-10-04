313582f36c86b48d7140dd2ce0847699b7eb6db5
Zaikov Evgeni <evgeniikas9@gmail.com>
2021-10-04 15:41:09 +0300

    feat: did common-character

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,7 +13,9 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default class DepthCalculator {
-  calculateDepth(/* arr */) {
+  calculateDepth(/*arr*/) {
+    
+
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
